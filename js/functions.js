d6e34bed4c22285ba18f5439eebfe8f98b1dd6ec
Alexsander <alexsander.Du.customs@gmail.com>
2024-07-13 14:04:04 +0300

    Испраляет ошибки/оптимизирует задачи

--- js/functions.js
+++ js/functions.js
@@ -1,66 +1,26 @@
-const checkStringLength = (newString, maxStringLength) => newString.length <= maxStringLength;
 
-checkStringLength('helloy', 6);
+const isLengthValid = (str, maxLength) => str.length <= maxLength;
+isLengthValid('helloy', 6);
 
-const isPolindrome = (newString = '') => {
-  const result = newString.toString().replaceAll(' ', '').toUpperCase();
-  let reversNewString = '';
+const isPolindrome = (str) => {
+  str = String(str).toUpperCase().replaceAll(' ', '');
+  let reverseStr = '';
 
-  for (let i = 0; i < result; i++) {
-    if (result[i] !== result[result.length - i - 1]){
+  for(let i = str.length - 1; i >= 0; i--) {
+    reverseStr += str[i];
+
+    if (str[i] !== str[str.length - i - 1]){
       return false;
     }
   }
 
-  for (let i = result.length - 1; i >= 0; i--) {
-    reversNewString += result[i];
-  }
-
-  return reversNewString === result;
+  return str === reverseStr;
 };
-
 isPolindrome();
 
-// Функция принимает строку, извлекает содержащиеся в ней цифры от 0 до 9 и возвращает их в виде целого положительного числа.
-// Если в строке нет ни одной цифры, функция должна вернуть NaN:
+const toRetrieveNumber = (str) => {
+  str = String(str).replaceAll(/\D+/g, '');
 
-// console.log(toRetrieveNumber(2023 год'))('2023 год'); // 2023
-// имяФункции('ECMAScript 2022'); // 2022
-// имяФункции('1 кефир, 0.5 батона'); // 105
-// имяФункции('агент 007'); // 7
-// имяФункции('а я томат'); // NaN
-// имяФункции(2023); // 2023
-// имяФункции(-1);   // 1
-// имяФункции(1.5);  // 15
-
-// Если хотите усложнить задание, предусмотрите случай, когда вместо строки приходит число.
-// Обратите внимание, что возвращать функция по-прежнему должна только целые положительные числа:
-
-// Что использовать?
-// Для решения этой задачи вам пригодятся:
-
-// цикл — для перебора полученной строки,
-// функция parseInt() — для превращения в число отдельных символов и результирующей строки,
-// функция Number.isNaN() — чтобы проверить, получилось ли превратить символ в число,
-// метод toString() — на случай, если в качестве параметра пришло число.
-
-
-const toRetrieveNumber = (newString) => {
-  let result = String(newString).replace(/\D/g,' ');
-
-  if (result < 0) {
-    result = result * -1;
-  }
-
-  return parseInt(result, 10);
+  return parseInt(str, 10);
 };
 toRetrieveNumber();
-
-console.log(toRetrieveNumber('2023 год'));
-console.log(toRetrieveNumber('ECMAScript 2022'));
-console.log(toRetrieveNumber('1 кефир, 0.5 батона'));
-console.log(toRetrieveNumber('агент 007'));
-console.log(toRetrieveNumber('а я томат'));
-console.log(toRetrieveNumber(2023));
-console.log(toRetrieveNumber(-1));
-console.log(Math.trunc(toRetrieveNumber(1.5)));
