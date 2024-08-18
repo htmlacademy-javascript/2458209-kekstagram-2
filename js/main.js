0b0d6ad0a6269f3c842d78a4bb5a95586d4950cd
Alexsander <alexsander.Du.customs@gmail.com>
2024-08-18 16:50:22 +0300

    дорабатывает домашнее задание

--- js/main.js
+++ js/main.js
@@ -1,40 +1,32 @@
-// В файле main.js напишите необходимые функции для создания массива из 25 сгенерированных объектов. Каждый объект массива — описание фотографии, опубликованной пользователем.
+//25 сгенерированных объектов. Каждый объект массива — описание фотографии, опубликованной пользователем.
 const SIMILAR_PHOTO_USER_COUNT = 25;
-// Структура каждого объекта должна быть следующей:
 
 // id, число — идентификатор опубликованной фотографии. Это число от 1 до 25. Идентификаторы не должны повторяться.
-const ID_PUBLISHED_PHOTO = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25];
+const idPublishedPhoto = [];
 
-// url, строка — адрес картинки вида photos/{{i}}.jpg, где {{i}} — это число от 1 до 25. Адреса картинок не должны повторяться.
+const getRandomId = (min, max) => {
+  const number = Math.floor(min + Math.random() * (max - min));
 
-const PICTURE_ADDRESS_URL = [
-  'photos/1.jpg',
-  'photos/2.jpg',
-  'photos/3.jpg',
-  'photos/4.jpg',
-  'photos/5.jpg',
-  'photos/6.jpg',
-  'photos/7.jpg',
-  'photos/8.jpg',
-  'photos/9.jpg',
-  'photos/10.jpg',
-  'photos/11.jpg',
-  'photos/12.jpg',
-  'photos/13.jpg',
-  'photos/14.jpg',
-  'photos/15.jpg',
-  'photos/16.jpg',
-  'photos/17.jpg',
-  'photos/18.jpg',
-  'photos/19.jpg',
-  'photos/20.jpg',
-  'photos/21.jpg',
-  'photos/22.jpg',
-  'photos/23.jpg',
-  'photos/24.jpg',
-  'photos/25.jpg',
-];
+  if (idPublishedPhoto.includes(number)) {
+    return getRandomId(min, max);
+  } else {
+    idPublishedPhoto.push(number);
+    return number;
+  }
+};
+// url, строка — адрес картинки. Адреса картинок не должны повторяться.
+const urlPublishedPhoto = [];
 
+const getRandomUrl = (min, max) => {
+  const number = Math.floor(min + Math.random() * (max - min));
+
+  if (urlPublishedPhoto.includes(number)) {
+    return getRandomUrl(min, max);
+  } else {
+    urlPublishedPhoto.push(number);
+    return number;
+  }
+};
 // description, строка — описание фотографии. Описание придумайте самостоятельно.
 const PHOTO_DESCRIPTION = [
   'фото замка',
@@ -63,7 +55,30 @@ const PHOTO_DESCRIPTION = [
   'картина хозяина замка',
   'картина хозяина замка с семьей',
 ];
-
+// Имена авторов также должны быть случайными. Набор имён для комментаторов составьте сами. Подставляйте случайное имя в поле name.
+const NAME_USERS = [
+  'Артём',
+  'Александр',
+  'Фиофан',
+  'Надежда',
+  'Кристюша',
+  'Аня',
+  'Слава',
+  'Кирилл',
+  'Лиза',
+  'Лена',
+  'Оксана',
+  'Лика',
+];
+// Для формирования текста комментария — message — вам необходимо взять одно или два случайных предложения из представленных ниже:
+const MESSAGE_USERS = [
+  'Всё отлично!',
+  'В целом всё неплохо. Но не всё.',
+  'Когда вы делаете фотографию, хорошо бы убирать палец из кадра. В конце концов это просто непрофессионально.',
+  'Моя бабушка случайно чихнула с фотоаппаратом в руках и у неё получилась фотография лучше.',
+  'Я поскользнулся на банановой кожуре и уронил фотоаппарат на кота и у меня получилась фотография лучше.',
+  'Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?!'
+];
 // likes, число — количество лайков, поставленных фотографии. Случайное число от 15 до 200.
 
 const getRandomInteger = (a, b) => {
@@ -72,25 +87,34 @@ const getRandomInteger = (a, b) => {
   const result = Math.random() * (upper - lower + 1) + lower;
   return Math.floor(result);
 };
-
 // comments, массив объектов — список комментариев, оставленных другими пользователями к этой фотографии. Количество комментариев к каждой фотографии — случайное число от 0 до 30. Все комментарии генерируются случайным образом.
 
 // У каждого комментария есть идентификатор — id — любое число. Идентификаторы не должны повторяться.
 
 // Поле avatar — это строка, значение которой формируется по правилу img/avatar-{{случайное число от 1 до 6}}.svg. Аватарки подготовлены в директории img.
 
-// Для формирования текста комментария — message — вам необходимо взять одно или два случайных предложения из представленных ниже:
-
-
-// Имена авторов также должны быть случайными. Набор имён для комментаторов составьте сами. Подставляйте случайное имя в поле name.
-
 const getRandomArrayElement = (elements) => elements[getRandomInteger(0, elements.length - 1)];
 
 const createDescriptionPhotoUser = () => ({
-  id: getRandomArrayElement(ID_PUBLISHED_PHOTO),
-  pictureAddress: getRandomArrayElement(PICTURE_ADDRESS_URL),
+  id: getRandomId(1, 26),
+  pictureAddress: `photos/${getRandomUrl(1, 26)}.jpg`,
   photoDescription: getRandomArrayElement(PHOTO_DESCRIPTION),
+  likes: getRandomInteger(15, 200),
+  comments: {
+    idComments: getRandomId(0, 999),
+    avatar: `img/avatar-${getRandomInteger(1, 6)}.svg`,
+    message: getRandomArrayElement(MESSAGE_USERS),
+    name: getRandomArrayElement(NAME_USERS),
+  },
 });
 
 const similarPhotoUser = Array.from({length: SIMILAR_PHOTO_USER_COUNT}, createDescriptionPhotoUser);
 console.log(similarPhotoUser);
+
+
+
+
+
+
+
+
