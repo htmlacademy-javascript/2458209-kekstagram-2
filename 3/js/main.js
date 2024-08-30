4b297d7bcd72ff531d7c82293be631dcfab5c538
Keks <keks@htmlacademy.ru>
2024-08-30 12:09:55 +0300

    :heavy_check_mark: Сборка #3

--- 3/js/main.js
+++ 3/js/main.js
@@ -1,8 +1,6 @@
 const MAX_PHOTOS = 25;
 const MIN_COMMENTS = 0;
 const MAX_COMMENTS = 30;
-const MIN_NUMBER_PHOTO = 1;
-const MAX_NUMBER_PHOTO = 25;
 const MIN_NUMBER_LIKES = 15;
 const MAX_NUMBER_LIKES = 200;
 const MIN_NUMBER_AVATARS = 1;
@@ -10,7 +8,7 @@ const MAX_NUMBER_AVATARS = 6;
 const MIN_ID_COMMENTS = 1;
 const MAX_ID_COMMENTS = 999;
 
-const PHOTOS_DESCRIPTION = [
+const PHOTO_DESCRIPTIONS = [
   'фото замка',
   'фото лужайки перед замком',
   'фото заката на фоне замка',
@@ -51,6 +49,10 @@ const NAME_USERS = [
   'Лена',
   'Оксана',
   'Лика',
+  'Карл',
+  'Иван',
+  'Ксюша',
+  'Джон',
 ];
 
 const MESSAGE_USERS = [
@@ -62,59 +64,40 @@ const MESSAGE_USERS = [
   'Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?!'
 ];
 
-function createGallery (length) {
+const getRandomInteger = (min, max) => {
+  const lower = Math.ceil(Math.min(min, max));
+  const upper = Math.floor(Math.max(min, max));
+  const result = Math.random() * (upper - lower + 1) + lower;
 
-  const getRandomInteger = (min, max) => {
-    const lower = Math.ceil(Math.min(min, max));
-    const upper = Math.floor(Math.max(min, max));
-    const result = Math.random() * (upper - lower + 1) + lower;
-    return Math.floor(result);
-  };
+  return Math.floor(result);
+};
 
-  function createRandom (min, max) {
-    const previousValues = [];
+const getRandomArrayElement = (elements) => elements[getRandomInteger(0, elements.length - 1)];
 
-    return function () {
+const createComment = function() {
 
-      let currentValue = getRandomInteger(min, max);
-      if (previousValues.length >= (max - min + 1)) {
-        return null;
-      }
-
-      while (previousValues.includes(currentValue)) {
-        currentValue = getRandomInteger(min, max);
-      }
-      previousValues.push(currentValue);
-
-      return currentValue;
-    };
-  }
-  const generateIdPhotos = createRandom(MIN_NUMBER_PHOTO, MAX_NUMBER_PHOTO);
-  const generatePictureAddress = createRandom(MIN_NUMBER_PHOTO, MAX_NUMBER_PHOTO);
-  const generateLikes = createRandom(MIN_NUMBER_LIKES, MAX_NUMBER_LIKES);
-  const generatIdComments = createRandom(MIN_ID_COMMENTS, MAX_ID_COMMENTS);
-  const generatComments = createRandom(MIN_COMMENTS, MAX_COMMENTS);
-
-  const getRandomArrayElement = (elements) => elements[getRandomInteger(0, elements.length - 1)];
-
-  const createComment = () => ({
-    idComments: generatIdComments(),
+  return {
+    id: getRandomInteger(MIN_ID_COMMENTS, MAX_ID_COMMENTS),
     avatar: `img/avatar-${getRandomInteger(MIN_NUMBER_AVATARS, MAX_NUMBER_AVATARS)}.svg`,
     message: getRandomArrayElement(MESSAGE_USERS),
     name: getRandomArrayElement(NAME_USERS),
-  });
+  };
+};
 
-  const createPhoto = () => ({
-    id: generateIdPhotos(),
-    pictureAddress: `photos/${generatePictureAddress()}.jpg`,
-    photoDescription: getRandomArrayElement(PHOTOS_DESCRIPTION),
-    likes: generateLikes(),
-    comments: Array.from({length: generatComments()}, createComment),
-  });
+const createPhoto = function(_, index) {
+  const photoId = index + 1;
 
-  length = Array.from({length: MAX_PHOTOS}, createPhoto);
+  return {
+    id: photoId,
+    url: `photos/${photoId}.jpg`,
+    description: getRandomArrayElement(PHOTO_DESCRIPTIONS),
+    likes: getRandomInteger(MIN_NUMBER_LIKES, MAX_NUMBER_LIKES),
+    comment: Array.from({length: getRandomInteger(MIN_COMMENTS, MAX_COMMENTS)}, createComment),
+  };
+};
 
-  return length;
-}
+const createGallery = function() {
+  return Array.from({length: MAX_PHOTOS}, createPhoto);
+};
 
-createGallery();
+createGallery(MAX_PHOTOS);
