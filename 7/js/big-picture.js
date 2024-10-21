298434d326806b48a39c594108ef8b5b5ded08c9
Keks <keks@htmlacademy.ru>
2024-10-21 18:01:57 +0300

    :heavy_check_mark: Сборка #7

--- 7/js/big-picture.js
+++ 7/js/big-picture.js
@@ -1,6 +1,6 @@
-import {isEscapeKey} from './utils.js';
-import DATA from './data.js';
+import * as DATA from './data.js';
 
+const objectData = Object.create(DATA);
 const sectionBigPicture = document.querySelector('.big-picture');
 const bigPictureImg = sectionBigPicture.querySelector('.big-picture__img').querySelector('img');
 const likesCounter = sectionBigPicture.querySelector('.likes-count');
@@ -9,18 +9,16 @@ const commentSocial = sectionBigPicture.querySelector('.social__comments');
 const commentSocialTemplate = commentSocial.querySelector('.social__comment');
 const comentDescription = sectionBigPicture.querySelector('.social__caption');
 const commentTotalCounter = document.querySelector('.social__comment-total-count');
+const counterComments = 0;
 
-let comments;
+let comments = [];
 
 const onBigPictureCancel = () => {
-  if (isEscapeKey) {
-    closePhoto();
-  }
+  closePhoto();
 };
 
 const onPhotoEscKeydown = (evt) => {
-  if (isEscapeKey(evt)) {
-    evt.preventDefault();
+  if (evt.key === 'Escape') {
     closePhoto();
   }
 };
@@ -41,7 +39,7 @@ bigPictureCancel.addEventListener('click', () => {
   closePhoto();
 });
 
-const renderComment = (comment) => {
+const createComment = (comment) => {
   const socialComment = commentSocialTemplate.cloneNode(true);
 
   socialComment.querySelector('.social__picture').src = comment.avatar;
@@ -54,30 +52,25 @@ const renderComment = (comment) => {
 const renderComments = () => {
   const socialCommentsFragment = document.createDocumentFragment();
 
-  comments.splice(0, DATA.MAX_COMMENTS).forEach((item) => {
-    socialCommentsFragment.append(renderComment(item));
+  comments.splice(counterComments, objectData.MAX_COMMENTS).forEach((item) => {
+    socialCommentsFragment.append(createComment(item));
   });
 
-  commentSocial.appendChild(socialCommentsFragment);
+  commentSocial.append(socialCommentsFragment);
 };
 
-const renderModal = (currentPhoto) => {
-  bigPictureImg.src = currentPhoto.url;
-  likesCounter.textContent = currentPhoto.likes;
-  comentDescription.textContent = currentPhoto.description;
-
-  renderComments();
-};
-
-export const openBigPicture = (currentPhoto) => {
+export const openBigPicture = (photoData) => {
   showModal();
 
-  comments = [...currentPhoto.comment];
+  comments = [...photoData.comment];
   commentSocial.innerHTML = '';
-  commentTotalCounter.textContent = currentPhoto.comment;
+  commentTotalCounter.textContent = photoData.comment;
+  bigPictureImg.src = photoData.url;
+  likesCounter.textContent = photoData.likes;
+  comentDescription.textContent = photoData.description;
 
-  renderModal(currentPhoto);
+  renderComments();
 
-  bigPictureCancel.addEventListener('click', closePhoto);
+  bigPictureCancel.addEventListener('click', onBigPictureCancel);
   document.addEventListener('keydown', onPhotoEscKeydown);
 };
